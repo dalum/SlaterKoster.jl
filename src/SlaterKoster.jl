156fdd101e2b84e9d51d3a4ef13cbae2f58e4920
Sakse Dalum <dalum@users.noreply.github.com>
2018-10-09 23:07:10 +0800

    Add optional datatype to `read`

--- src/SlaterKoster.jl
+++ src/SlaterKoster.jl
@@ -25,7 +25,8 @@ Parse `filepath` and return a Slater-Koster table derived from its
 contents.
 
 """
-function read(filepath::String; element_names=(nothing, nothing))
+read(filepath; element_names=(nothing, nothing)) = read(Float64, filepath, element_names=element_names)
+function read(T::Type, filepath::String; element_names=(nothing, nothing))
     dirname, filename = splitdir(filepath)
     if element_names === (nothing, nothing)
         # Try to derive the element names from the file name by
@@ -41,149 +42,149 @@ function read(filepath::String; element_names=(nothing, nothing))
         if startswith(lines[1], "@")
             return HomoNuclearTable(
                 first_element_name,
-                _readextendedformat_homonuclear(lines)...)
+                _readextendedformat_homonuclear(T, lines)...)
         else
             return HomoNuclearTable(
                 first_element_name,
-                _readsimpleformat_homonuclear(lines)...)
+                _readsimpleformat_homonuclear(T, lines)...)
         end
     else
         if startswith(lines[1], "@")
             return HeteroNuclearTable(
                 first_element_name,
                 second_element_name,
-                _readextendedformat_heteronuclear(lines)...)
+                _readextendedformat_heteronuclear(T, lines)...)
         else
             return HeteroNuclearTable(
                 first_element_name,
                 second_element_name,
-                _readsimpleformat_heteronuclear(lines)...)
+                _readsimpleformat_heteronuclear(T, lines)...)
         end
     end
 end
 
-function _readsimpleformat_homonuclear(lines)
-    grid_dist, n_grid_points = parse.((Float64, Int), split(lines[1]))
+function _readsimpleformat_homonuclear(T, lines)
+    grid_dist, n_grid_points = parse.((T, Int), split(lines[1]))
 
     energy_table = DataFrame(
-        Ed = Float64[], Ep = Float64[], Es = Float64[],
-        SPE = Float64[],
-        Ud = Float64[], Up = Float64[], Us = Float64[],
-        fd = Float64[], fp = Float64[], fs = Float64[])
-    push!(energy_table, parse.(Float64, split(lines[2])))
+        Ed = T[], Ep = T[], Es = T[],
+        SPE = T[],
+        Ud = T[], Up = T[], Us = T[],
+        fd = T[], fp = T[], fs = T[])
+    push!(energy_table, parse.(T, split(lines[2])))
 
     mass_table = DataFrame(
-        mass = Float64[],
-        c2 = Float64[], c3 = Float64[], c4 = Float64[], c5 = Float64[],
-        c6 = Float64[], c7 = Float64[], c8 = Float64[], c9 = Float64[],
-        rcut = Float64[],
-        d1 = Float64[], d2 = Float64[], d3 = Float64[], d4 = Float64[], d5 = Float64[],
-        d6 = Float64[], d7 = Float64[], d8 = Float64[], d9 = Float64[], d10 = Float64[])
-    push!(mass_table, parse.(Float64, split(lines[3])))
+        mass = T[],
+        c2 = T[], c3 = T[], c4 = T[], c5 = T[],
+        c6 = T[], c7 = T[], c8 = T[], c9 = T[],
+        rcut = T[],
+        d1 = T[], d2 = T[], d3 = T[], d4 = T[], d5 = T[],
+        d6 = T[], d7 = T[], d8 = T[], d9 = T[], d10 = T[])
+    push!(mass_table, parse.(T, split(lines[3])))
 
     integral_table = DataFrame(
-        dist = Float64[],
-        Hdd0 = Float64[], Hdd1 = Float64[], Hdd2 = Float64[], Hpd0 = Float64[], Hpd1 = Float64[], Hpp0 = Float64[],
-        Hpp1 = Float64[], Hsd0 = Float64[], Hsp0 = Float64[], Hss0 = Float64[],
-        Sdd0 = Float64[], Sdd1 = Float64[], Sdd2 = Float64[], Spd0 = Float64[], Spd1 = Float64[], Spp0 = Float64[],
-        Spp1 = Float64[], Ssd0 = Float64[], Ssp0 = Float64[], Sss0 = Float64[])
+        dist = T[],
+        Hdd0 = T[], Hdd1 = T[], Hdd2 = T[], Hpd0 = T[], Hpd1 = T[], Hpp0 = T[],
+        Hpp1 = T[], Hsd0 = T[], Hsp0 = T[], Hss0 = T[],
+        Sdd0 = T[], Sdd1 = T[], Sdd2 = T[], Spd0 = T[], Spd1 = T[], Spp0 = T[],
+        Spp1 = T[], Ssd0 = T[], Ssp0 = T[], Sss0 = T[])
 
     for (i, line) in enumerate(lines[4:4+n_grid_points-1])
-        push!(integral_table, vcat([i*grid_dist], parse.(Float64, split(line))))
+        push!(integral_table, vcat([i*grid_dist], parse.(T, split(line))))
     end
     return energy_table, mass_table, integral_table
 end
 
-function _readsimpleformat_heteronuclear(lines)
-    grid_dist, n_grid_points = parse.((Float64, Int), split(lines[1]))
+function _readsimpleformat_heteronuclear(T, lines)
+    grid_dist, n_grid_points = parse.((T, Int), split(lines[1]))
 
     mass_table = DataFrame(
-        mass = Float64[],
-        c2 = Float64[], c3 = Float64[], c4 = Float64[], c5 = Float64[],
-        c6 = Float64[], c7 = Float64[], c8 = Float64[], c9 = Float64[],
-        rcut = Float64[],
-        d1 = Float64[], d2 = Float64[], d3 = Float64[], d4 = Float64[], d5 = Float64[],
-        d6 = Float64[], d7 = Float64[], d8 = Float64[], d9 = Float64[], d10 = Float64[])
-    push!(mass_table, parse.(Float64, split(lines[2])))
+        mass = T[],
+        c2 = T[], c3 = T[], c4 = T[], c5 = T[],
+        c6 = T[], c7 = T[], c8 = T[], c9 = T[],
+        rcut = T[],
+        d1 = T[], d2 = T[], d3 = T[], d4 = T[], d5 = T[],
+        d6 = T[], d7 = T[], d8 = T[], d9 = T[], d10 = T[])
+    push!(mass_table, parse.(T, split(lines[2])))
 
     integral_table = DataFrame(
-        dist = Float64[],
-        Hdd0 = Float64[], Hdd1 = Float64[], Hdd2 = Float64[], Hpd0 = Float64[], Hpd1 = Float64[], Hpp0 = Float64[],
-        Hpp1 = Float64[], Hsd0 = Float64[], Hsp0 = Float64[], Hss0 = Float64[],
-        Sdd0 = Float64[], Sdd1 = Float64[], Sdd2 = Float64[], Spd0 = Float64[], Spd1 = Float64[], Spp0 = Float64[],
-        Spp1 = Float64[], Ssd0 = Float64[], Ssp0 = Float64[], Sss0 = Float64[])
+        dist = T[],
+        Hdd0 = T[], Hdd1 = T[], Hdd2 = T[], Hpd0 = T[], Hpd1 = T[], Hpp0 = T[],
+        Hpp1 = T[], Hsd0 = T[], Hsp0 = T[], Hss0 = T[],
+        Sdd0 = T[], Sdd1 = T[], Sdd2 = T[], Spd0 = T[], Spd1 = T[], Spp0 = T[],
+        Spp1 = T[], Ssd0 = T[], Ssp0 = T[], Sss0 = T[])
 
     for (i, line) in enumerate(lines[3:3+n_grid_points-1])
-        push!(integral_table, vcat([i*grid_dist], parse.(Float64, split(line))))
+        push!(integral_table, vcat([i*grid_dist], parse.(T, split(line))))
     end
     return mass_table, integral_table
 end
 
-function _readextendedformat_homonuclear(lines)
-    grid_dist, n_grid_points = parse.((Float64, Int), split(lines[2]))
+function _readextendedformat_homonuclear(T, lines)
+    grid_dist, n_grid_points = parse.((T, Int), split(lines[2]))
 
     energy_table = DataFrame(
-        Ef = Float64[], Ed = Float64[], Ep = Float64[], Es = Float64[],
-        SPE = Float64[],
-        Uf = Float64[], Ud = Float64[], Up = Float64[], Us = Float64[],
-        ff = Float64[], fd = Float64[], fp = Float64[], fs = Float64[])
-    push!(energy_table, parse.(Float64, split(lines[3])))
+        Ef = T[], Ed = T[], Ep = T[], Es = T[],
+        SPE = T[],
+        Uf = T[], Ud = T[], Up = T[], Us = T[],
+        ff = T[], fd = T[], fp = T[], fs = T[])
+    push!(energy_table, parse.(T, split(lines[3])))
 
     mass_table = DataFrame(
-        mass = Float64[],
-        c2 = Float64[], c3 = Float64[], c4 = Float64[], c5 = Float64[],
-        c6 = Float64[], c7 = Float64[], c8 = Float64[], c9 = Float64[],
-        rcut = Float64[],
-        d1 = Float64[], d2 = Float64[], d3 = Float64[], d4 = Float64[], d5 = Float64[],
-        d6 = Float64[], d7 = Float64[], d8 = Float64[], d9 = Float64[], d10 = Float64[])
-    push!(mass_table, parse.(Float64, split(lines[4])))
+        mass = T[],
+        c2 = T[], c3 = T[], c4 = T[], c5 = T[],
+        c6 = T[], c7 = T[], c8 = T[], c9 = T[],
+        rcut = T[],
+        d1 = T[], d2 = T[], d3 = T[], d4 = T[], d5 = T[],
+        d6 = T[], d7 = T[], d8 = T[], d9 = T[], d10 = T[])
+    push!(mass_table, parse.(T, split(lines[4])))
 
     integral_table = DataFrame(
-        dist = Float64[],
-        Hff0 = Float64[], Hff1 = Float64[], Hff2 = Float64[], Hff3 = Float64[],
-        Hdf0 = Float64[], Hdf1 = Float64[], Hdf2 = Float64[], Hdd0 = Float64[],
-        Hdd1 = Float64[], Hdd2 = Float64[], Hpf0 = Float64[], Hpf1 = Float64[],
-        Hpd0 = Float64[], Hpd1 = Float64[], Hpp0 = Float64[], Hpp1 = Float64[],
-        Hsf0 = Float64[], Hsd0 = Float64[], Hsp0 = Float64[], Hss0 = Float64[],
-        Sdd1 = Float64[], Sdd2 = Float64[], Sff0 = Float64[], Sff1 = Float64[],
-        Sff2 = Float64[], Sff3 = Float64[], Sdf0 = Float64[], Sdf1 = Float64[],
-        Sdf2 = Float64[], Sdd0 = Float64[], Spf0 = Float64[], Spf1 = Float64[],
-        Spd0 = Float64[], Spd1 = Float64[], Spp0 = Float64[], Spp1 = Float64[],
-        Ssf0 = Float64[], Ssd0 = Float64[], Ssp0 = Float64[], Sss0 = Float64[])
+        dist = T[],
+        Hff0 = T[], Hff1 = T[], Hff2 = T[], Hff3 = T[],
+        Hdf0 = T[], Hdf1 = T[], Hdf2 = T[], Hdd0 = T[],
+        Hdd1 = T[], Hdd2 = T[], Hpf0 = T[], Hpf1 = T[],
+        Hpd0 = T[], Hpd1 = T[], Hpp0 = T[], Hpp1 = T[],
+        Hsf0 = T[], Hsd0 = T[], Hsp0 = T[], Hss0 = T[],
+        Sdd1 = T[], Sdd2 = T[], Sff0 = T[], Sff1 = T[],
+        Sff2 = T[], Sff3 = T[], Sdf0 = T[], Sdf1 = T[],
+        Sdf2 = T[], Sdd0 = T[], Spf0 = T[], Spf1 = T[],
+        Spd0 = T[], Spd1 = T[], Spp0 = T[], Spp1 = T[],
+        Ssf0 = T[], Ssd0 = T[], Ssp0 = T[], Sss0 = T[])
 
     for (i, line) in enumerate(lines[5:5+n_grid_points-1])
-        push!(integral_table, vcat([i*grid_dist], parse.(Float64, split(line))))
+        push!(integral_table, vcat([i*grid_dist], parse.(T, split(line))))
     end
     return energy_table, mass_table, integral_table
 end
 
-function _readextendedformat_heteronuclear(lines)
-    grid_dist, n_grid_points = parse.((Float64, Int), split(lines[2]))
+function _readextendedformat_heteronuclear(T, lines)
+    grid_dist, n_grid_points = parse.((T, Int), split(lines[2]))
 
     mass_table = DataFrame(
-        mass = Float64[],
-        c2 = Float64[], c3 = Float64[], c4 = Float64[], c5 = Float64[],
-        c6 = Float64[], c7 = Float64[], c8 = Float64[], c9 = Float64[],
-        rcut = Float64[],
-        d1 = Float64[], d2 = Float64[], d3 = Float64[], d4 = Float64[], d5 = Float64[],
-        d6 = Float64[], d7 = Float64[], d8 = Float64[], d9 = Float64[], d10 = Float64[])
-    push!(mass_table, parse.(Float64, split(lines[3])))
+        mass = T[],
+        c2 = T[], c3 = T[], c4 = T[], c5 = T[],
+        c6 = T[], c7 = T[], c8 = T[], c9 = T[],
+        rcut = T[],
+        d1 = T[], d2 = T[], d3 = T[], d4 = T[], d5 = T[],
+        d6 = T[], d7 = T[], d8 = T[], d9 = T[], d10 = T[])
+    push!(mass_table, parse.(T, split(lines[3])))
 
     integral_table = DataFrame(
-        dist = Float64[],
-        Hff0 = Float64[], Hff1 = Float64[], Hff2 = Float64[], Hff3 = Float64[],
-        Hdf0 = Float64[], Hdf1 = Float64[], Hdf2 = Float64[], Hdd0 = Float64[],
-        Hdd1 = Float64[], Hdd2 = Float64[], Hpf0 = Float64[], Hpf1 = Float64[],
-        Hpd0 = Float64[], Hpd1 = Float64[], Hpp0 = Float64[], Hpp1 = Float64[],
-        Hsf0 = Float64[], Hsd0 = Float64[], Hsp0 = Float64[], Hss0 = Float64[],
-        Sdd1 = Float64[], Sdd2 = Float64[], Sff0 = Float64[], Sff1 = Float64[],
-        Sff2 = Float64[], Sff3 = Float64[], Sdf0 = Float64[], Sdf1 = Float64[],
-        Sdf2 = Float64[], Sdd0 = Float64[], Spf0 = Float64[], Spf1 = Float64[],
-        Spd0 = Float64[], Spd1 = Float64[], Spp0 = Float64[], Spp1 = Float64[],
-        Ssf0 = Float64[], Ssd0 = Float64[], Ssp0 = Float64[], Sss0 = Float64[])
+        dist = T[],
+        Hff0 = T[], Hff1 = T[], Hff2 = T[], Hff3 = T[],
+        Hdf0 = T[], Hdf1 = T[], Hdf2 = T[], Hdd0 = T[],
+        Hdd1 = T[], Hdd2 = T[], Hpf0 = T[], Hpf1 = T[],
+        Hpd0 = T[], Hpd1 = T[], Hpp0 = T[], Hpp1 = T[],
+        Hsf0 = T[], Hsd0 = T[], Hsp0 = T[], Hss0 = T[],
+        Sdd1 = T[], Sdd2 = T[], Sff0 = T[], Sff1 = T[],
+        Sff2 = T[], Sff3 = T[], Sdf0 = T[], Sdf1 = T[],
+        Sdf2 = T[], Sdd0 = T[], Spf0 = T[], Spf1 = T[],
+        Spd0 = T[], Spd1 = T[], Spp0 = T[], Spp1 = T[],
+        Ssf0 = T[], Ssd0 = T[], Ssp0 = T[], Sss0 = T[])
 
     for (i, line) in enumerate(lines[4:4+n_grid_points-1])
-        push!(integral_table, vcat([i*grid_dist], parse.(Float64, split(line))))
+        push!(integral_table, vcat([i*grid_dist], parse.(T, split(line))))
     end
     return mass_table, integral_table
 end
